b69eea32b833c2174c77643786f4af0b9b617ec6
Alexis Vignot <alexisvignot@hotmail.fr>
2024-05-13 17:06:25 +0200

    feat: Ajout de du router et du store à terminer

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './components/App/App';
-
 import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './components/App/App';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
