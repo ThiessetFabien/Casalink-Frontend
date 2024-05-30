130b60c1438d2916fd02240083c91824cd8cedd0
Conrath Téo <teo.conrath.pro@gmail.com>
2024-05-30 17:11:09 +0200

    improvment: css modal update profile desktop

--- src/components/Modals/Profile/updateProfileForm.modale.tsx
+++ src/components/Modals/Profile/updateProfileForm.modale.tsx
@@ -8,6 +8,7 @@ import {
   actionUploadProfileImage,
 } from '../../../store/thunks/changeProfile';
 import actionGetMembers from '../../../store/thunks/checkProfile';
+import './updateProfileForm.scss';
 
 interface EditProfileModalProps {
   profile: MemberStateI;
@@ -136,101 +137,107 @@ function EditProfileModal({ profile, closeModal }: EditProfileModalProps) {
           <h1 className="update_modal_title">
             Modifier le profil de {updatedProfile.name}
           </h1>
-          <div className="profile_field">
-            <label htmlFor="name">Nom</label>
-            <input
-              value={updatedProfile.name}
-              onChange={handleChange}
-              className="input_required"
-              type="text"
-              name="name"
-              id="name"
-              required
-            />
-          </div>
-          <div className="profile_field">
-            <label htmlFor="birthdate">Date de naissance</label>
-            <input
-              value={format(new Date(updatedProfile.birthdate), 'yyyy-MM-dd')}
-              onChange={handleDateChange}
-              className="input_required"
-              type="date"
-              name="birthdate"
-              id="birthdate"
-              required
-            />
-          </div>
-          <div className="profile_field">
-            <label htmlFor="image">Modifier sa Photo de profil</label>
-            <input
-              onChange={handleImageChangeBase64}
-              className="profile_field_image"
-              type="file"
-              accept=".jpeg, .jpg, .png, .webp"
-              name="image"
-              id="image"
-            />
-          </div>
-          <div className="profile_field">
-            <label htmlFor="role">Type de profil</label>
-            <div className="profile_field_role">
+          <div className="update_modal_container_inputs">
+            <div className="profile_field">
+              <label htmlFor="name">Nom</label>
               <input
-                type="radio"
-                name="role"
-                id="Adulte"
-                value="adult"
+                value={updatedProfile.name}
                 onChange={handleChange}
-                checked={updatedProfile.role === 'adult'}
-                className="input_checkbox_adult"
+                className="input_required"
+                type="text"
+                name="name"
+                id="name"
+                required
               />
-              <span className="input_checkbox_adult_title">Adulte</span>
-              <input
-                type="radio"
-                name="role"
-                id="Enfant"
-                value="child"
-                onChange={handleChange}
-                checked={updatedProfile.role === 'child'}
-                className="input_checkbox_child"
-              />
-              <span className="input_checkbox_child_title">Enfant</span>
             </div>
-          </div>
-          {isAdultChecked && (
-            <>
-              <div className="profile_field">
-                <label htmlFor="email">Email</label>
-                <input
-                  value={updatedProfile.email}
-                  onChange={handleChange}
-                  className="input_email"
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder={updatedProfile.email || 'Entrer votre email'}
-                />
+            <div className="profile_field birdthdate">
+              <label htmlFor="birthdate">Date de naissance</label>
+              <input
+                value={format(new Date(updatedProfile.birthdate), 'yyyy-MM-dd')}
+                onChange={handleDateChange}
+                className="input_required"
+                type="date"
+                name="birthdate"
+                id="birthdate"
+                required
+              />
+            </div>
+            <div className="profile_field image">
+              <label htmlFor="image">Modifier sa Photo de profil</label>
+              <input
+                onChange={handleImageChangeBase64}
+                className="profile_field_image"
+                type="file"
+                accept=".jpeg, .jpg, .png, .webp"
+                name="image"
+                id="image"
+              />
+            </div>
+            <div className="profile_field radio">
+              <label htmlFor="role">Type de profil</label>
+              <div className="profile_field_role">
+                <div className="profile_field_role_adult">
+                  <input
+                    type="radio"
+                    name="role"
+                    id="Adulte"
+                    value="adult"
+                    onChange={handleChange}
+                    checked={updatedProfile.role === 'adult'}
+                    className="input_checkbox_adult"
+                  />
+                  <span className="input_checkbox_adult_title">Adulte</span>
+                </div>
+                <div className="profile_field_role_child">
+                  <input
+                    type="radio"
+                    name="role"
+                    id="Enfant"
+                    value="child"
+                    onChange={handleChange}
+                    checked={updatedProfile.role === 'child'}
+                    className="input_checkbox_child"
+                  />
+                  <span className="input_checkbox_child_title">Enfant</span>
+                </div>
               </div>
-              <div className="profile_field">
-                <label htmlFor="pin">Code Pin</label>
-                <input
-                  value={updatedProfile.pin}
-                  onChange={handleChange}
-                  className="input_pin"
-                  type="password"
-                  name="pin"
-                  placeholder="****"
-                  required
-                />
-              </div>
-            </>
-          )}
+            </div>
+            {isAdultChecked && (
+              <>
+                <div className="profile_field">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    value={updatedProfile.email}
+                    onChange={handleChange}
+                    className="input_email"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder={updatedProfile.email || 'Entrer votre email'}
+                  />
+                </div>
+                <div className="profile_field pin">
+                  <label htmlFor="pin">Code Pin</label>
+                  <input
+                    value={updatedProfile.pin}
+                    onChange={handleChange}
+                    className="input_pin"
+                    type="password"
+                    name="pin"
+                    placeholder="****"
+                    required
+                  />
+                </div>
+              </>
+            )}
 
-          <span className="errorMessage">{errorMessages}</span>
-          <div className="update_modal_profile_containerButton">
-            <button type="submit">Enregistrer les modifications</button>
-            <button type="button" onClick={() => closeModal()}>
-              Annuler
-            </button>
+            <span className="errorMessage">{errorMessages}</span>
+            <div className="update_modal_profile_containerButton">
+              <button type="submit">Enregistrer les modifications</button>
+              <button type="button" onClick={() => closeModal()}>
+                Annuler
+              </button>
+            </div>
           </div>
         </form>
       </div>
