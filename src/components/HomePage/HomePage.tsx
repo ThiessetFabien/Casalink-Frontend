5ab8063432324bf4a7b1985bfab37e73a0a112fe
Alexis Vignot <alexisvignot@hotmail.fr>
2024-06-22 16:19:16 +0200

    fix: date task

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -140,8 +140,8 @@ function HomePage() {
     memberTarget: number,
     memberRole: 'adult' | 'child'
   ) => {
-    const start = format(startUnserielized, 'yyyy-MM-dd HH:mm');
-    const end = format(endUnserielized, 'yyyy-MM-dd HH:mm');
+    const start = startUnserielized.toISOString();
+    const end = endUnserielized.toISOString();
     const resultActionModifyTask = dispatch(
       actionModifyTask({
         id,
